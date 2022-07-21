c3e176fa9c632366bf22dbadf9ac9a7dbf0c823b
Mohit Upadhyay <55775426+MohitUpa@users.noreply.github.com>
2022-07-21 13:38:27 +0530

    final

--- src/app/am-chart/am-chart.component.ts
+++ src/app/am-chart/am-chart.component.ts
@@ -58,43 +58,43 @@ export class AmChartComponent implements AfterViewInit {
       );
 
       // Add labels and controls
-      cont.children.push(
-        am5.Label.new(root, {
-          centerY: am5.p50,
-          text: "Map"
-        })
-      );
+      // cont.children.push(
+      //   am5.Label.new(root, {
+      //     centerY: am5.p50,
+      //     text: "Map"
+      //   })
+      // );
 
-      let switchButton = cont.children.push(
-        am5.Button.new(root, {
-          themeTags: ["switch"],
-          centerY: am5.p50,
-          icon: am5.Circle.new(root, {
-            themeTags: ["icon"]
-          })
-        })
-      );
+      // let switchButton = cont.children.push(
+      //   am5.Button.new(root, {
+      //     themeTags: ["switch"],
+      //     centerY: am5.p50,
+      //     icon: am5.Circle.new(root, {
+      //       themeTags: ["icon"]
+      //     })
+      //   })
+      // );
 
-      switchButton.on("active", function () {
-        if (!switchButton.get("active")) {
-          chart.set("projection", am5map.geoMercator());
-          chart.set("panY", "translateY");
-          chart.set("rotationY", 0);
-          backgroundSeries.mapPolygons.template.set("fillOpacity", 0);
-        } else {
-          chart.set("projection", am5map.geoOrthographic());
-          chart.set("panY", "rotateY")
+      // switchButton.on("active", function () {
+      //   if (!switchButton.get("active")) {
+      //     chart.set("projection", am5map.geoMercator());
+      //     chart.set("panY", "translateY");
+      //     chart.set("rotationY", 0);
+      //     backgroundSeries.mapPolygons.template.set("fillOpacity", 0);
+      //   } else {
+      //     chart.set("projection", am5map.geoOrthographic());
+      //     chart.set("panY", "rotateY")
 
-          backgroundSeries.mapPolygons.template.set("fillOpacity", 0.1);
-        }
-      });
+      //     backgroundSeries.mapPolygons.template.set("fillOpacity", 0.1);
+      //   }
+      // });
 
-      cont.children.push(
-        am5.Label.new(root, {
-          centerY: am5.p50,
-          text: "Globe"
-        })
-      );
+      // cont.children.push(
+      //   am5.Label.new(root, {
+      //     centerY: am5.p50,
+      //     text: "Globe"
+      //   })
+      // );
 
       // Create series for background fill
       // https://www.amcharts.com/docs/v5/charts/map-chart/map-polygon-series/#Background_polygon
@@ -141,36 +141,38 @@ export class AmChartComponent implements AfterViewInit {
             tooltipY: 0,
             fill: colorset.next(),
             strokeOpacity: 0,
-            tooltipText: "{title}"
+            tooltipHTML: `
+            {title}<br>
+            <img src="{flag}" width="30px">`
           })
         );
 
-        let circle2 = container.children.push(
-          am5.Circle.new(root, {
-            radius: 4,
-            tooltipY: 0,
-            fill: colorset.next(),
-            strokeOpacity: 0,
-            tooltipText: "{title}"
-          })
-        );
+        // let circle2 = container.children.push(
+        //   am5.Circle.new(root, {
+        //     radius: 4,
+        //     tooltipY: 0,
+        //     fill: colorset.next(),
+        //     strokeOpacity: 0,
+        //     tooltipText: "{title}"
+        //   })
+        // );
 
-        circle.animate({
-          key: "scale",
-          from: 1,
-          to: 5,
-          duration: 600,
-          easing: am5.ease.out(am5.ease.cubic),
-          loops: Infinity
-        });
-        circle.animate({
-          key: "opacity",
-          from: 1,
-          to: 0,
-          duration: 600,
-          easing: am5.ease.out(am5.ease.cubic),
-          loops: Infinity
-        });
+        // circle.animate({
+        //   key: "scale",
+        //   from: 1,
+        //   to: 5,
+        //   duration: 600,
+        //   easing: am5.ease.out(am5.ease.cubic),
+        //   loops: Infinity
+        // });
+        // circle.animate({
+        //   key: "opacity",
+        //   from: 1,
+        //   to: 0,
+        //   duration: 600,
+        //   easing: am5.ease.out(am5.ease.cubic),
+        //   loops: Infinity
+        // });
 
         return am5.Bullet.new(root, {
           sprite: container
@@ -181,106 +183,125 @@ export class AmChartComponent implements AfterViewInit {
         {
           title: "Brussels",
           latitude: 50.8371,
-          longitude: 4.3676
+          longitude: 4.3676,
+          flag: 'https://image.shutterstock.com/image-vector/belgium-flag-260nw-386009581.jpg',
         },
         {
           title: "Copenhagen",
           latitude: 55.6763,
-          longitude: 12.5681
+          longitude: 12.5681,
+          flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Flag_of_Denmark.svg/640px-Flag_of_Denmark.svg.png',
         },
         {
           title: "Paris",
           latitude: 48.8567,
-          longitude: 2.351
+          longitude: 2.351,
+          flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Flag_of_Paris_with_coat_of_arms.svg/1280px-Flag_of_Paris_with_coat_of_arms.svg.png',
         },
         {
           title: "Reykjavik",
           latitude: 64.1353,
-          longitude: -21.8952
+          longitude: -21.8952,
+          flag: 'https://cdn.britannica.com/85/1485-004-94C3DEDA/Flag-Iceland.jpg',
         },
         {
           title: "Moscow",
           latitude: 55.7558,
-          longitude: 37.6176
+          longitude: 37.6176,
+          flag: 'https://ak.picdn.net/shutterstock/videos/1053933155/thumb/1.jpg',
         },
         {
           title: "Madrid",
           latitude: 40.4167,
-          longitude: -3.7033
+          longitude: -3.7033,
+          flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Flag_of_the_Community_of_Madrid.svg/800px-Flag_of_the_Community_of_Madrid.svg.png',
         },
         {
           title: "London",
           latitude: 51.5002,
           longitude: -0.1262,
-          url: "http://www.google.co.uk"
+          url: "http://www.google.co.uk",
+          flag: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1200px-Flag_of_the_United_Kingdom.svg.png',
         },
         {
           title: "Peking",
           latitude: 39.9056,
-          longitude: 116.3958
+          longitude: 116.3958,
+          flag: 'https://cdn.britannica.com/90/7490-004-BAD4AA72/Flag-China.jpg',
         },
         {
           title: "New Delhi",
           latitude: 28.6353,
-          longitude: 77.225
+          longitude: 77.225,
+          flag: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/255px-Flag_of_India.svg.png',
         },
         {
           title: "Tokyo",
           latitude: 35.6785,
           longitude: 139.6823,
+          flag: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/1200px-Flag_of_Japan.svg.png',
           url: "http://www.google.co.jp"
         },
         {
           title: "Ankara",
           latitude: 39.9439,
-          longitude: 32.856
+          longitude: 32.856,
+          flag: 'https://cdn.pixabay.com/photo/2021/11/14/14/26/turkey-6794740_960_720.png',
         },
         {
           title: "Buenos Aires",
           latitude: -34.6118,
-          longitude: -58.4173
+          longitude: -58.4173,
+          flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Bandera_de_la_Provincia_de_Buenos_Aires.svg/1200px-Bandera_de_la_Provincia_de_Buenos_Aires.svg.png',
         },
         {
           title: "Brasilia",
           latitude: -15.7801,
-          longitude: -47.9292
+          longitude: -47.9292,
+          flag: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/640px-Flag_of_Brazil.svg.png',
         },
         {
           title: "Ottawa",
           latitude: 45.4235,
-          longitude: -75.6979
+          longitude: -75.6979,
+          flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Flag_of_Ottawa%2C_Ontario.svg/1200px-Flag_of_Ottawa%2C_Ontario.svg.png',
         },
         {
           title: "Washington",
           latitude: 38.8921,
-          longitude: -77.0241
+          longitude: -77.0241,
+          flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Flag_of_Washington.svg/255px-Flag_of_Washington.svg.png',
         },
         {
           title: "Kinshasa",
           latitude: -4.3369,
-          longitude: 15.3271
+          longitude: 15.3271,
+          flag: 'https://cdn.britannica.com/94/7194-004-5FA84A72/Flag-Democratic-Republic-of-the-Congo.jpg',
         },
         {
           title: "Cairo",
           latitude: 30.0571,
-          longitude: 31.2272
+          longitude: 31.2272,
+          flag: 'https://flagpedia.net/data/flags/w1600/eg.png',
         },
         {
           title: "Pretoria",
           latitude: -25.7463,
-          longitude: 28.1876
+          longitude: 28.1876,
+          flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Flag_of_Pretoria%2C_South_Africa.svg/300px-Flag_of_Pretoria%2C_South_Africa.svg.png',
         }
       ];
 
       for (var i = 0; i < cities.length; i++) {
         let city = cities[i];
-        addCity(city.longitude, city.latitude, city.title);
+        addCity(city.longitude, city.latitude, city.title, city.flag);
       }
 
-      function addCity(longitude: number, latitude: number, title: string) {
+      function addCity(longitude: number, latitude: number, title: string, flag: string) {
         pointSeries.data.push({
           geometry: { type: "Point", coordinates: [longitude, latitude] },
-          title: title
+          title: title,
+          flag:flag
         });
       }
 
