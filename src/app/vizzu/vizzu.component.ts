a62c7934bfab982b9c88ff4e094aa0e1bbcb4526
Mohit <mohitu531@gmail.com>
2022-07-29 18:02:07 +0530

    test

--- src/app/vizzu/vizzu.component.ts
+++ src/app/vizzu/vizzu.component.ts
@@ -56,11 +56,12 @@ export class VizzuComponent implements OnInit, AfterViewInit {
     am4core.useTheme(am4themes_animated);
 
     var chart = am4core.create("chartdiv1", am4plugins_forceDirected.ForceDirectedTree);
-    chart.legend = new am4charts.Legend();
+
 
     var networkSeries = chart.series.push(new am4plugins_forceDirected.ForceDirectedSeries())
 
     networkSeries.data = [{
+      id: '1',
       name: 'Bangladesh',
       value: 1,
       fixed: true,
@@ -81,6 +82,7 @@ export class VizzuComponent implements OnInit, AfterViewInit {
         name: 'Bangladesh', value: 1
       }]
     }, {
+      id: '2',
       name: 'Australia',
       color: ("#00306C"),
       fixed: true,
@@ -120,6 +122,7 @@ export class VizzuComponent implements OnInit, AfterViewInit {
 
       }]
     }, {
+      id: '3',
       name: 'Rawada',
       color: ("#4A92EC"),
       fixed: true,
@@ -153,6 +156,7 @@ export class VizzuComponent implements OnInit, AfterViewInit {
       }
       ]
     }, {
+      id: '4',
       name: 'colombia',
       color: ("#4AEC9B"),
       fixed: true,
@@ -174,25 +178,25 @@ export class VizzuComponent implements OnInit, AfterViewInit {
       }]
 
     }, {
-      name: 'Roasted',
+      name: '',
       fixed: true,
       value: 1,
       x: am4core.percent(150),
       y: am4core.percent(40),
       children: [{
-        name: 'Pipe Tobacco', value: 4
+        name: '', value: 3
       }]
 
     }];
 
     networkSeries.dataFields.linkWith = "linkWith";
     networkSeries.dataFields.name = "name";
-    networkSeries.dataFields.id = "name";
+    networkSeries.dataFields.id = "id";
     networkSeries.dataFields.value = "value";
     networkSeries.dataFields.children = "children";
     networkSeries.dataFields.fixed = "fixed";
     networkSeries.dataFields.color = "color";
-    
+    // networkSeries.nodes.template.width = 100;
 
     networkSeries.nodes.template.propertyFields.x = "x";
     networkSeries.nodes.template.propertyFields.y = "y";
@@ -205,18 +209,21 @@ export class VizzuComponent implements OnInit, AfterViewInit {
     // networkSeries.maxLevels = 3;
     networkSeries.nodes.template.label.hideOversized = true;
     networkSeries.nodes.template.label.truncate = true;
-    networkSeries.links.template.distance = 0;
+    networkSeries.links.template.distance = -1;
     networkSeries.links.template.disabled = true;
     networkSeries.nodes.template.interactionsEnabled = false;
 
-    networkSeries.links.template.strokeWidth = 0;
+    networkSeries.nodes.template.strokeWidth = 0;
     networkSeries.links.template.strokeOpacity = 0;
     networkSeries.nodes.template.label.fill = am4core.color("#fff");
 
- 
+    networkSeries.nodes.template.outerCircle.strokeOpacity = 0;
+    networkSeries.nodes.template.outerCircle.fillOpacity = 0;
 
-
-
+    var title = chart.titles.create();
+    title.text = "Health Governance";
+    title.fontSize = 25;
+    title.marginBottom = 30;
   }
 
   myChart() {
@@ -244,36 +251,30 @@ export class VizzuComponent implements OnInit, AfterViewInit {
     }));
 
 
-    // Add legend
-    // https://www.amcharts.com/docs/v5/charts/xy-chart/legend-xy-series/
-    var legend = chart.children.push(am5.Legend.new(root, {
-      centerX: am5.p50,
-      x: am5.p50,
-    }))
 
     let data = [{
-      'text':'Australia',
+      'text': 'Australia',
       "year": "2012",
       "income": 98,
       "columnConfig": {
         fill: am5.color(0x00306C),
       }
     }, {
-      'text':'Rawada',
+      'text': 'Rawada',
       "year": "2011",
       "income": 73,
       "columnConfig": {
         fill: am5.color(0x4A92EC),
       }
     }, {
-      'text':'colombia',
+      'text': 'colombia',
       "year": "2010",
       "income": 50,
       "columnConfig": {
         fill: am5.color(0x4AEC9B),
       }
     }, {
-      'text':'bangladesh',
+      'text': 'bangladesh',
       "year": "2009",
       "income": 25,
       "columnConfig": {
@@ -289,21 +290,22 @@ export class VizzuComponent implements OnInit, AfterViewInit {
         categoryField: "year",
         renderer: am5xy.AxisRendererY.new(root, {
           cellStartLocation: 0.1,
-          cellEndLocation: 0.9
+          cellEndLocation: 0.9,
         }),
-        tooltip: am5.Tooltip.new(root, {}),
       })
     );
 
+  
     yAxis.data.setAll(data);
 
     let xAxis = chart.xAxes.push(
       am5xy.ValueAxis.new(root, {
         min: 0,
-        renderer: am5xy.AxisRendererX.new(root, {}),
+        renderer: am5xy.AxisRendererX.new(root, {
+
+        }),
       })
     );
-
 
     // Add series
     // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
@@ -326,6 +328,34 @@ export class VizzuComponent implements OnInit, AfterViewInit {
       strokeOpacity: 0
     });
 
+    series1.bullets.push(function() {
+      return am5.Bullet.new(root, {
+        locationX: 1,
+        locationY: 0.5,
+        sprite: am5.Label.new(root, {
+          centerY: am5.p50,
+          text: "{valueX}%",
+          populateText: true
+        })
+      });
+    });
+
+    series1.bullets.push(function() {
+      return am5.Bullet.new(root, {
+        locationX: 1,
+        locationY: 0.5,
+        sprite: am5.Label.new(root, {
+          centerX: am5.p100,
+          centerY: am5.p50,
+          text: "{text}",
+          fill: am5.color(0xffffff),
+          populateText: true
+        })
+      });
+    });
+
+   
+
     // Add cursor
     // https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
     let cursor = chart.set("cursor", am5xy.XYCursor.new(root, {
@@ -334,16 +364,15 @@ export class VizzuComponent implements OnInit, AfterViewInit {
     cursor.lineX.set("visible", false);
     cursor.lineY.set("visible", false);
 
+    // series1.labelsContainer.setAll("width", 100);
+    // series1.ticks.template.setAll("location", 0.6);
 
     series1.data.setAll(data);
 
     // Make stuff animate on load
     // https://www.amcharts.com/docs/v5/concepts/animations/
     series1.appear();
-    chart.appear(1000, 100);
-
-
-
+    chart.appear(1000, 100);  
   }
 
 
